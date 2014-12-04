b8010ffd5be11a349ea62816add7d482095295a0
Cameron Marlow <cameron@cameronmarlow.com>
2014-12-03 16:37:52 -0800

    Translate url to appropriate options

--- flask_elasticsearch.py
+++ flask_elasticsearch.py
@@ -1,6 +1,9 @@
 from flask import current_app
 from elasticsearch import Elasticsearch as PyElasticSearch
-
+try:
+    from urllib.parse import urlparse
+except ImportError:
+    from urlparse import urlparse
 
 class ElasticSearch(object):
     """
@@ -10,14 +13,38 @@ class ElasticSearch(object):
         if app is not None:
             self.init_app(app, **kwargs)
 
+    @classmethod
+    def url_to_options(cls, url):
+        """Translate qualified URL into Elasticsearch options"""
+        parsed = urlparse(url)
+        opts = {
+            'host': parsed.hostname
+        }
+        if parsed.port:
+            opts['port'] = parsed.port
+
+        if parsed.scheme == 'https':
+            opts['use_ssl'] = True
+            if not parsed.port:
+                opts['port'] = 443
+        elif parsed.scheme == 'http' and not parsed.port:
+            opts['port'] = 80
+
+        if parsed.username and parsed.password:
+            opts['http_auth'] = parsed.username + ':' + parsed.password
+        return opts
+
     def init_app(self, app, **kwargs):
         app.config.setdefault('ELASTICSEARCH_URL', 'http://localhost:9200/')
+        opts = self.url_to_options(app.config.get('ELASTICSEARCH_URL'))
+        kwargs.update(opts)
 
         # using the app factory pattern _app_ctx_stack.top is None so what
         # do we register on? app.extensions looks a little hackish (I don't
         # know flask well enough to be sure), but that's how it's done in
         # flask-pymongo so let's use it for now.
-        app.extensions['elasticsearch'] = PyElasticSearch(app.config['ELASTICSEARCH_URL'], **kwargs)
+        app.extensions['elasticsearch'] = PyElasticSearch(
+            app.config['ELASTICSEARCH_URL'], **kwargs)
 
     def __getattr__(self, item):
         if not 'elasticsearch' in current_app.extensions.keys():
